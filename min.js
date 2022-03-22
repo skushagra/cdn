756887339ab80c74f076817811718c86816b73ec
Kushagra S <kushagra.rigel@gmail.com>
2022-03-22 23:23:11 +0530

    Update min.js

--- min.js
+++ min.js
@@ -1,17 +1,21 @@
+function directLog(log){
+    console.log(log);
+}
+
 function DjangoAjaxViewCall(view, method, dataset){
     console.log(view);
     console.log(method);
     return "success";
-//     called_url = `{% url '${view}' %}`
-//     $.ajax({
-//         url: called_url,
-//         type: method,
-//         data: dataset,
-//         success: (data) => {
-//             return data;
-//         },
-//         failure: (error) => {
-//             return error;
-//         }
-//     });
+    called_url = `{% url '${view}' %}`
+    $.ajax({
+        url: called_url,
+        type: method,
+        data: dataset,
+        success: (data) => {
+            return data;
+        },
+        failure: (error) => {
+            return error;
+        }
+    });
 }
